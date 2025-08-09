301aaf75e98d29867d3970555f13b54b77a52827
Safouane Ahendouz <safwanahendouz241@gmail.com>
2025-08-09 10:53:12 +0100

    Form_JS

---
+++ script.js
@@ -0,0 +1,31 @@
+const home = document.querySelector('li')
+home.classList.add('li-active')
+home.children[0].classList.add('line-active')
+console.log(home)
+
+
+// Function switch nav bar
+function switchNav(num) {
+
+    const lists = document.querySelectorAll('li');
+    console.log(lists)
+    
+    lists.forEach((list)=>{
+        list.classList.remove('li-active')
+        list.children[0].classList.remove('line-active')
+       
+        
+    })
+
+    lists[num].classList.add('li-active')
+    lists[num].children[0].classList.add('line-active')
+
+    console.log(lists[num])
+
+}
+
+// Go to link
+
+function goToCv() {
+    window.open('https://github.com/new')
+}
